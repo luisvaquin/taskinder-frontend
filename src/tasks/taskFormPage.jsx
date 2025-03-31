d53ef4ee3461ec47e13bb1c217a8570988bf70a3
Luis Vaquin <luisrodrigo2330@gmail.com>
2025-03-31 15:16:18 -0600

    errors

---
+++ src/tasks/taskFormPage.jsx
@@ -0,0 +1,13 @@
+import { useForm } from "react-hook-form";
+
+function TaskFormPage() {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default TaskFormPage;
